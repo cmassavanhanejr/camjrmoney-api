ff98d7cd6585b27023b8947093e9d379e3540f93
Carlos  A.Massavanhane Jr <cmassavanhanejr@gmail.com>
2021-04-13 15:25:22 +0200

    init
    iniciando projecto

---
+++ src/main/java/com/camjr/camjrmoneyapi/CamjrmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.camjr.camjrmoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamjrmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamjrmoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camjr/camjrmoneyapi/CamjrmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camjr.camjrmoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamjrmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
